29824268ed069905e8aa04726aacd43d0bb6ef25
石井賢二 <espoir.ka@gmail.com>
2014-10-01 12:53:37 +0900

    動作再生用のサンプルコードを実装（UIWebViewにyoutube埋め込み）

---
+++ anpi_1/anpi_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  anpi_1
+//
+//  Created by 石井賢二 on 2014/10/01.
+//  Copyright (c) 2014年 石井賢二. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
