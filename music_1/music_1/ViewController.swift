d4f9c2c8a3abd94d0987d7a73ba15c9e9466cd12
石井賢二 <espoir.ka@gmail.com>
2014-10-01 12:54:56 +0900

    update

--- music_1/music_1/ViewController.swift
+++ music_1/music_1/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webview: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.webview.frame = self.view.bounds;
+        //self.webview.delegate = self;
+        //self.view.addSubview(self.webview)
+        
+        var url = NSURL.URLWithString("http://www.youtube.com/embed/OW0S0zOJeR8?feature=player_detailpage&playsinline=1")
+        var urlRequest: NSURLRequest = NSURLRequest(URL: url)
+        self.webview.allowsInlineMediaPlayback = true;
+        self.webview.loadRequest(urlRequest)
     }
 
     override func didReceiveMemoryWarning() {
